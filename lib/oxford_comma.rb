338934a0221652dae3913a7faf8b7fe78d24d907
Ruth <osukukuruth@gmail.com>
2023-07-07 16:58:42 -0400

    Done Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
-def oxford_comma(array)
 
-end
+    def oxford_comma(array)
+        case array.size
+        when 0
+          ""
+        when 1
+          array[0]
+        when 2
+          "#{array[0]} and #{array[1]}"
+        else
+          last_element = array.pop
+          "#{array.join(", ")}, and #{last_element}"
+        end
+      end
+
+      puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+# Output: "fiddleheads, okra, and kohlrabi"
+
+puts oxford_comma(["apple"])
+# Output: "apple"
+
+puts oxford_comma(["apple", "banana"])
+# Output: "apple and banana"
+
+puts oxford_comma([])
+# Output: ""
+
+      
